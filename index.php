6c538c675b0f44a8d6a7b9273c52017dcf7e406d
Henrique <henriquepalmaifrs@gmail.com>
2024-03-22 18:35:25 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,14 @@
+$users = {
+    1 => "Jorge Fiqueiredo",
+    4 => "Amanda de Souza",
+    10 => "Francisco",
+    11 => "Andressa",
+    20 => "Rosalinda"
+};
+$bornDate = {
+    1 => "01/03/1995",
+    4 => "05/07/2000",
+    10 => "21/04/1996",
+    11 => "16/07/1988",
+    20 => "11/11/2000"
+}
