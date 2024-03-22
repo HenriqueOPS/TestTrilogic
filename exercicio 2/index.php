c8e35787ef156b4a1c519cbf9ca262890605bf3b
Henrique <henriquepalmaifrs@gmail.com>
2024-03-22 18:51:23 -0300

    Pastas

--- exercicio 2/index.php
+++ exercicio 2/index.php
@@ -1,17 +1,19 @@
-$users = {
+$users = [
     1 => "Jorge Fiqueiredo",
     4 => "Amanda de Souza",
     10 => "Francisco",
     11 => "Andressa",
     20 => "Rosalinda"
-};
-$bornDate = {
+];
+
+$bornDate = [
     1 => "01/03/1995",
     4 => "05/07/2000",
     10 => "21/04/1996",
     11 => "16/07/1988",
     20 => "11/11/2000"
-}
+];
+
 function calcularIdade($dataNascimento) {
     $dataAtual = new DateTime();
     $dataNascimento = DateTime::createFromFormat('d/m/Y', $dataNascimento);
